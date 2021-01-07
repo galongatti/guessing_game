ca516b66f1c475ae1c78847c9307a937c6680410
Gabriel Longatti <galongatti@hotmail.com>
2021-01-07 15:01:16 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+
+    println!("Guess the number!");
+
+    let _secret_number: i32 = rand::thread_rng().gen_range(1..100);
+
+    loop{
+        
+        println!("Please inpute your guess.");
+
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: i32 = match guess.trim()
+            .parse::<i32>(){
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Insert a number!");
+                    continue;
+                }
+            };
+
+        println!("You guessed: {}", guess);
+
+        //println!("The secret number is : {}", _secret_number);
+    
+    
+        match guess.cmp(&_secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+    }
+
+   
+}
